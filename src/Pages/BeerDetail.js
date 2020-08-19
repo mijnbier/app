dbb2ffcc3980015f40dd5c9a85f6268909d51c16
DVermeer <duncan.vermeer@gmail.com>
2020-08-19 19:19:33 +0000

    Add Drink + Stock Toevoegen button

--- src/Pages/BeerDetail.js
+++ src/Pages/BeerDetail.js
@@ -1,16 +1,21 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import { data } from "../Data";
+import React, { useState } from 'react';
 
 export default function BeerDetail() {
   const { id } = useParams();
   const beer = data.find((item) => item.brand === id);
+  const stock = beer.stock
+  const [count, setCount] = useState(stock);
+
 
   return (
     <div>
       <h3>{beer.brand}</h3>
       <div>Label: {beer.labels.join(' - ')} </div>
-      <div>Voorraad: {beer.stock}</div>
+      <div>Voorraad: {count}</div>
+      <button onClick={() => setCount(count - 1)}> Drink </button>
+      <button onClick={() => setCount(count + 1)}> Toevoegen</button>
     </div>
   );
 }
