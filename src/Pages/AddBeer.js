49216ffc802a3e3b05fe3afe58f5df30ed74c39b
DVermeer <duncan.vermeer@gmail.com>
2020-08-21 20:45:23 +0200

    Btn + Page add beer

---
+++ src/Pages/AddBeer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AddBeer() {
+
+    return (
+        <div>
+            <h3>Pagina Bier toevoegen</h3>
+        </div>
+    );
+} 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "../pub.svg";
+import Button from '@material-ui/core/Button';
 import { data } from "../Data";
+import { Link } from 'react-router-dom';
 import "./Home.css";
 import BeerList from "../Components/BeerList";
 
@@ -9,7 +11,8 @@ export default function Home() {
     <header>
       <img src={logo} className="logo" alt="logo" />
       <p>Mijn Bier App</p>
+      <Button variant="contained" color="primary" component={Link} to="/add-beer">Nieuw Bier toevoegen</Button >
       <BeerList name="Duncan" data={data}></BeerList>
-    </header>
+    </header >
   );
 }
