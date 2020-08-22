06e12fa44cb7680cb200220e99817298495feb08
DVermeer <duncan.vermeer@gmail.com>
2020-08-22 19:36:01 +0200

    Form add beer

--- src/Pages/AddBeer.js
+++ src/Pages/AddBeer.js
@@ -4,25 +4,56 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from '@material-ui/core/MenuItem';
-
+import Button from "@material-ui/core/Button";
 
 export default function AddBeer() {
-
     return (
         <div>
             <h3>Pagina Bier toevoegen</h3>
-            <h4>Naam:</h4>
-            <h4>Bierstijl:</h4>
-
-            <FormControl variant="outlined" >
-                <InputLabel id="selectbierstijl">Bierstijl</InputLabel>
-                <Select>
-                    <MenuItem value={'Dubbel'}>Dubbel</MenuItem>
-                    <MenuItem value={'Tripel'}>Tripel</MenuItem>
-                    <MenuItem value={'IPA'}>IPA</MenuItem>
-                </Select>
+            <FormControl variant="outlined" style={{ minWidth: 120 }}>
+                <TextField
+                    id="beername"
+                    label="Bier"
+                    variant="outlined"
+                />
+                <TextField
+                    id="brewery"
+                    label="Brouwerij"
+                    variant="outlined"
+                />
+                <FormControl variant="outlined" >
+                    <InputLabel id="selectbeerstyle">Bierstijl</InputLabel>
+                    <Select
+                        labelId="selectbeerstyle"
+                        id="beerstyle">
+                        <MenuItem value={'bock'}>Bock</MenuItem>
+                        <MenuItem value={'blond'}>Blond</MenuItem>
+                        <MenuItem value={'dubbel'}>Dubbel</MenuItem>
+                        <MenuItem value={'ipa'}>IPA</MenuItem>
+                        <MenuItem value={'porter'}>Porter</MenuItem>
+                        <MenuItem value={'stout'}>Stout</MenuItem>
+                        <MenuItem value={'tripel'}>Tripel</MenuItem>
+                        <MenuItem value={'weizen'}>Weizen</MenuItem>
+                        <MenuItem value={'witbier'}>Witbier</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField
+                    id="alcohol"
+                    label="Alcohol percentage"
+                    type="number"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-number"
+                    label="Aantal stuks"
+                    type="number"
+                    variant="outlined"
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                > Toevoegen</Button>
             </FormControl>
-        </div>
-
+        </div >
     );
 } 
