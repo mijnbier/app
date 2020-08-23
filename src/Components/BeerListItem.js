5e995816363ae80ea7e942063432f436ba29df51
DVermeer <duncan.vermeer@gmail.com>
2020-08-23 17:44:33 +0200

    Add bottle image to beerlistitem

--- src/Components/BeerListItem.js
+++ src/Components/BeerListItem.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Price from "./Price";
 import { Link } from "react-router-dom";
+import bottle from './beer-bottle.png';
 
 export default function BeerListItem(props) {
   return (
     <li>
-      Naam: <Link to={`/${props.id}`}>{props.brand}</Link>, {props.alcohol}%
+      <div>
+        <img src={bottle} alt="Beer Bottle" height="100" background="transparent"></img>
+      </div>
+
+      <div>
+        Naam: <Link to={`/${props.id}`}>{props.brand}</Link>, {props.alcohol}%
       alc. ({props.brewery}) <Price value={props.price} /> Voorraad:{" "}
-      {props.stock}x
+        {props.stock}x
+      </div>
     </li>
   );
 }
