362b767b48dfb153e9983187b17763936227dc41
Melvin Vermeer <melvin.vermeer@gmail.com>
2020-12-30 23:12:46 +0100

    Code styling

--- src/Pages/AddBeer.js
+++ src/Pages/AddBeer.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./AddBeer.css";
 import { useHistory, Link } from "react-router-dom";
-import FormControl from '@material-ui/core/FormControl';
+import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import db from "../Services/database";
 import "firebase/firestore";
-import InputAdornment from '@material-ui/core/InputAdornment';
+import InputAdornment from "@material-ui/core/InputAdornment";
 import logo from "../pub.svg";
 import firebase from "firebase";
 
@@ -18,53 +18,63 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
       Width: 200,
     },
-    '&.Mui-focused fieldset': {
-        borderColor: 'green',
-      },
+    "&.Mui-focused fieldset": {
+      borderColor: "green",
+    },
   },
 }));
 
 export default function AddBeer() {
-    const classes = useStyles();
-    const history = useHistory();
+  const classes = useStyles();
+  const history = useHistory();
 
-    const [name, setName] = React.useState("");
-    const [brewery, setBrewery] = React.useState("");
-    const [stock, setStock] = React.useState(1);
-    const [style, setStyle] = React.useState("");
-    const [alcohol, setAlcohol] = React.useState(null);
-    const [beerExists, setBeerExists] = React.useState(false);
+  const [name, setName] = React.useState("");
+  const [brewery, setBrewery] = React.useState("");
+  const [stock, setStock] = React.useState(1);
+  const [style, setStyle] = React.useState("");
+  const [alcohol, setAlcohol] = React.useState(null);
+  const [beerExists, setBeerExists] = React.useState(false);
 
-    const handleNameChange = async (e) => {
-        setName(e.target.value);
-        const result = await db.collection('beers').where("brand", "==", e.target.value).get()
-        setBeerExists(result.size === 1)
-    };
+  const handleNameChange = async (e) => {
+    setName(e.target.value);
+    const result = await db
+      .collection("beers")
+      .where("brand", "==", e.target.value)
+      .get();
+    setBeerExists(result.size === 1);
+  };
 
-    const handleStockChange = (e) => {
-        setStock(e.target.value);
-    };
-    const handleBreweryChange = (e) => {
-        setBrewery(e.target.value);
-    };
-    const handleStyleChange = (e) => {
-        setStyle(e.target.value);
-    };
-    const handleAlcoholChange = (e) => {
-        setAlcohol(e.target.value);
-    };
-    const onUpdate = async () => {
-        if(beerExists) {
-            const result = await db.collection('beers').where("brand", "==", name).get()
-            db.collection('beers').doc(result.docs[0].id).update({
-                stock: firebase.firestore.FieldValue.increment(parseInt(stock))
-            })
-        } else {
-            db.collection('beers').doc().set({ brand: name, price: 0, stock, brewery, alcohol, style })
-        }
-        
-        history.push("/");
+  const handleStockChange = (e) => {
+    setStock(e.target.value);
+  };
+  const handleBreweryChange = (e) => {
+    setBrewery(e.target.value);
+  };
+  const handleStyleChange = (e) => {
+    setStyle(e.target.value);
+  };
+  const handleAlcoholChange = (e) => {
+    setAlcohol(e.target.value);
+  };
+  const onUpdate = async () => {
+    if (beerExists) {
+      const result = await db
+        .collection("beers")
+        .where("brand", "==", name)
+        .get();
+      db.collection("beers")
+        .doc(result.docs[0].id)
+        .update({
+          stock: firebase.firestore.FieldValue.increment(parseInt(stock)),
+        });
+    } else {
+      db.collection("beers")
+        .doc()
+        .set({ brand: name, price: 0, stock, brewery, alcohol, style });
     }
+
+    history.push("/");
+  };
 
   return (
     <header>
@@ -74,13 +84,17 @@ export default function AddBeer() {
       <FormControl className={classes.root} variant="outlined">
         <TextField
           onChange={handleNameChange}
-          className={classes.root} 
+          className={classes.root}
           value={name}
           color={beerExists ? "secondary" : "primary"}
           id="brand"
           label="Bier"
           InputProps={{
-            endAdornment: <InputAdornment position="edge"><img hidden={!beerExists} src={logo}/></InputAdornment>,
+            endAdornment: (
+              <InputAdornment position="edge">
+                <img hidden={!beerExists} src={logo} />
+              </InputAdornment>
+            ),
           }}
         />
         <TextField
