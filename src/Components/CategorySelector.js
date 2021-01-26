cb1d8a18da77670812391c597a4e4c443a97a7e6
DVermeer <duncan.vermeer@gmail.com>
2021-01-26 19:00:40 +0100

    import beercategories to categoryselector

--- src/Components/CategorySelector.js
+++ src/Components/CategorySelector.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import beercategories from "./BeerCategory"
 
 export default  ({ beers }) =>
 
-    <Tabs >
+    <Tabs 
+    variant="scrollable"
+    scrollButtons="on">
         <Tab label="Alle" />
-        <Tab label="Blond"  />
-        <Tab label="Dubbel"  />
-        <Tab label="Tripel"  />
+        {beercategories.map(category =>  
+            (
+          <Tab key={category} label={category}/>)
+          )}
+        
     </Tabs>
 
