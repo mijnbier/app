e69a1bc9162e97335d48d4dc0febb1579def47b8
DVermeer <duncan.vermeer@gmail.com>
2021-01-27 19:54:40 +0100

    category selector working

--- src/Components/BeerList.js
+++ src/Components/BeerList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import BeerListItem from "./BeerListItem";
 
-function selectedCategory(beer, category) {
-  return beer.style === category
-}
-
-// function isOnStock(beer) {
-//   return beer.stock > 0
+// function selectedCategory(beer, category) {
+//   return beer.style === category
 // }
+
+function isOnStock(beer) {
+  return beer.stock > 0
+}
 
 function toListItem(item, index) {
   return (
@@ -18,7 +18,7 @@ function toListItem(item, index) {
 export default function BeerList(props, category) {
   return (
     <ul style={{ listStyleType: "none" }}>
-      {props.data.filter(selectedCategory).map(toListItem)}
+      {props.data.filter(isOnStock).map(toListItem)}
     </ul>
   );
 }
--- src/Components/CategorySelector.js
+++ src/Components/CategorySelector.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import beercategories from "../Data/BeerCategories"
+import React from "react";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import beercategories from "../Data/BeerCategories";
 
-// const [value, setValue] = React.useState(0);
-// const handleChange = (event, newValue) => {
-//     setValue(newValue);
-//   };
+export default () => {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-export default  ({ beers }) =>
-
+  return (
     <Tabs
-    value={0} 
-    // onChange={}
-    variant="scrollable"
-    scrollButtons="on">
-        <Tab label="Alle" />
-        {beercategories.map(category =>  
-            (
-          <Tab key={category} label={category}/>)
-          )}
-        
+      value={value}
+      onChange={handleChange}
+      variant="scrollable"
+      scrollButtons="on"
+    >
+      <Tab label="Alle" />
+      {beercategories.map((category) => (
+        <Tab key={category} label={category} />
+      ))}
     </Tabs>
-
+  );
+};
