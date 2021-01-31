616083bc3685faabd6daa0d84cbc709a74e87676
DVermeer <duncan.vermeer@gmail.com>
2021-01-31 19:48:32 +0100

    correct Popular (n.n.b.)

--- src/Components/PopularBeer.js
+++ src/Components/PopularBeer.js
@@ -1,18 +1,7 @@
 import React from "react";
 
-function isOnStock(beer) {
-  return beer.stock > 0;
-}
-
-function findMax() {
-}
-
 export default function PopularBeer(props) {
-  return (
-    <td>
-      
+  return <td>
       n.n.b.
-      {props.data.filter(isOnStock)}
-    </td>
-  );
+      </td>;
 }
