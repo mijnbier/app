4429eb90387684bd632016b230fdb043b1eb156d
DVermeer <duncan.vermeer@gmail.com>
2021-02-02 22:35:51 +0100

    install cors for setting token

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
 
-export default function Login() {
+
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(data => data.json())
+   }
+
+export default function Login({setToken}) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
   return (
     <div className="login-wrapper">
       <form>
         <label>
-          User: <input type="text" />
+          User: <input type="text" onChange={e => setUserName(e.target.value)}/>
         </label>
         <br></br>
         <label>
-          Pin: <input type="password" />
+          Pin: <input type="password" onChange={e => setPassword(e.target.value)}/>
         </label>
         <br></br>
         <button type="submit">Submit</button>
@@ -17,3 +32,7 @@ export default function Login() {
     </div>
   );
 }
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+  }
