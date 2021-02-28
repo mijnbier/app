fb1ca7b4795716e54f41a2c6182354fc2364970b
DVermeer <duncan.vermeer@gmail.com>
2021-02-28 18:26:36 +0100

    remove styles

--- src/Components/BeerList.js
+++ src/Components/BeerList.js
@@ -3,23 +3,6 @@ import BeerListItem from "./BeerListItem";
 import SwipeableViews from "react-swipeable-views";
 import beercategories from "../Data/BeerCategories";
 
-const styles = {
-  slide: {
-    padding: 15,
-    minHeight: 100,
-    color: "#fff",
-  },
-  slide1: {
-    background: "#FEA900",
-  },
-  slide2: {
-    background: "#B3DC4A",
-  },
-  slide3: {
-    background: "#6AC0FF",
-  },
-};
-
 function isOnStock(beer) {
   return beer.stock > 0;
 }
@@ -36,22 +19,22 @@ export default function BeerList(props) {
 
   function handleChangeIndex(index) {
     if (index === 0) {
-      console.log("0")
+      console.log("0");
       props.setCategory(undefined);
     } else {
-      console.log(beercategories[index - 1])
+      console.log(beercategories[index - 1]);
       props.setCategory(beercategories[index - 1]);
     }
   }
 
   return (
     <SwipeableViews index={index} onChangeIndex={handleChangeIndex}>
-      <div style={Object.assign({}, styles.slide, styles.slide1)}>
+      <div>
         <ul style={{ listStyleType: "none" }}>
           {props.data.filter(isOnStock).map(toListItem)}
         </ul>
       </div>
-      <div style={Object.assign({}, styles.slide, styles.slide2)}>
+      <div>
         <ul style={{ listStyleType: "none" }}>
           {props.data
             .filter(isOnStock)
@@ -59,7 +42,7 @@ export default function BeerList(props) {
             .map(toListItem)}
         </ul>
       </div>
-      <div style={Object.assign({}, styles.slide, styles.slide3)}>
+      <div>
         <ul style={{ listStyleType: "none" }}>
           {props.data
             .filter(isOnStock)
