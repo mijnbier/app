88852450a4209da103bf422d3d1181d25ff211de
DVermeer <duncan.vermeer@gmail.com>
2021-04-11 17:47:46 +0200

    work in progress

--- src/Components/DeleteBeerDialog.js
+++ src/Components/DeleteBeerDialog.js
@@ -9,10 +9,17 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import database from "../Services/database";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function AlertDialog() {
   const [open, setOpen] = React.useState(false);
-  const history = useHistory();
+  const [openSnackbar, setOpenSnackbar] = React.useState(false);
+  //const history = useHistory();
   const { id } = useParams();
 
   const handleClickOpen = () => {
@@ -23,14 +30,28 @@ export default function AlertDialog() {
     setOpen(false);
   };
 
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
   async function deleteBeer() {
-    await database.collection("beers").doc(id).delete();
+    //await database.collection("beers").doc(id).delete();
     setOpen(false);
-    history.push("/");
+    setOpenSnackbar(true);
+    //history.push("/");
   }
 
   return (
     <Fragment>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert onClose={handleCloseSnackbar} severity="success">
+          This is a success message!
+        </Alert>
+      </Snackbar>
       <Button color="primary" variant="contained" onClick={handleClickOpen}>
         Delete
       </Button>
