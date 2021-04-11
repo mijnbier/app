18e9891461bd62fcd147db4a11aaeaabd54ad2e4
Melvin Vermeer <melvin.vermeer@abcam.com>
2021-04-11 19:30:16 +0200

    Fix Snackbar - Alerts

--- src/Components/DeleteBeerDialog.js
+++ src/Components/DeleteBeerDialog.js
@@ -9,16 +9,10 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import database from "../Services/database";
-import Snackbar from "@material-ui/core/Snackbar";
-import MuiAlert from "@material-ui/lab/Alert";
 import useSnackBars from "../SnackBarConsumer";
-function Alert(props) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
 
 export default function AlertDialog() {
   const [open, setOpen] = React.useState(false);
-  const [openSnackbar, setOpenSnackbar] = React.useState(false);
   const history = useHistory();
   const { id } = useParams();
   const { addAlert } = useSnackBars();
@@ -31,31 +25,15 @@ export default function AlertDialog() {
     setOpen(false);
   };
 
-  const handleCloseSnackbar = () => {
-    setOpenSnackbar(false);
-  };
-
   async function deleteBeer() {
-    //await database.collection("beers").doc(id).delete();
+    await database.collection("beers").doc(id).delete();
     setOpen(false);
-    //setOpenSnackbar(true);
-
-    addAlert("Your profile is updated!");
-
+    addAlert("Bier succesvol verwijderd");
     history.push("/");
   }
 
   return (
     <Fragment>
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={6000}
-        onClose={handleCloseSnackbar}
-      >
-        <Alert onClose={handleCloseSnackbar} severity="success">
-          This is a success message!
-        </Alert>
-      </Snackbar>
       <Button color="primary" variant="contained" onClick={handleClickOpen}>
         Delete
       </Button>
--- src/Contexts/SnackBarProvider.js
+++ src/Contexts/SnackBarProvider.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState, useEffect, createContext } from "react";
-
-import SnackBar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 
 export const SnackBarContext = createContext();
 
@@ -9,10 +8,9 @@ const AUTO_DISMISS = 5000;
 
 export function SnackBarProvider({ children }) {
   const [alerts, setAlerts] = useState([]);
-
   const activeAlertIds = alerts.join(",");
+
   useEffect(() => {
-    console.log("asdasd");
     if (activeAlertIds.length > 0) {
       const timer = setTimeout(
         () => setAlerts((alerts) => alerts.slice(0, alerts.length - 1)),
@@ -22,20 +20,13 @@ export function SnackBarProvider({ children }) {
     }
   }, [activeAlertIds]);
 
-  const addAlert = (alert) =>
-    setAlerts((alerts) => {
-      console.log(alert);
-      return [alert, ...alerts];
-    });
-
-  const value = { addAlert };
+  const addAlert = (alert) => setAlerts((alerts) => [alert, ...alerts]);
 
   return (
-    <SnackBarContext.Provider value={value}>
+    <SnackBarContext.Provider value={{ addAlert }}>
       {children}
       {alerts.map((alert) => {
-        console.log("RENDER", alert);
-        return <SnackBar key={alert}>{alert}</SnackBar>;
+        return <Alert key={alert}>{alert}</Alert>;
       })}
     </SnackBarContext.Provider>
   );
